8419a5701ec9a4458514930f1932a90d52c4a90c
Alexey <apple230379@gmail.com>
2021-12-03 14:58:41 +0300

    3.2.2 Добавит новый компонент - заглушку - Создаем новый компонент - заглушку - По условию - нет задач - рендерим заглушку на страницу - Если задачи есть, то показываем компонент сортировки
    N.B. Чтобы проверить, что заглушка показывается, обнулите константу TASK_COUNT

--- src/main.js
+++ src/main.js
@@ -6,23 +6,21 @@ import LoadMoreButtonView from './view/load-more-button-view.js';
 import BoardView from './view/board-view.js';
 import SortView from './view/sort-view.js';
 import TaskListView from './view/task-list-view.js';
+import NoTaskView from './view/no-task-view.js';
 import {render, RenderPosition} from './render.js';
 import {generateTask} from './mock/task.js';
 import {generateFilter} from './mock/filter.js';
 
 const TASK_COUNT = 22;
 const TASK_COUNT_PER_STEP = 8;
-
 const tasks = Array.from({length: TASK_COUNT}, generateTask);
 const filters = generateFilter(tasks);
-
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 const renderTask = (taskListElement, task) => {
   const taskComponent = new TaskView(task);
   const taskEditComponent = new TaskEditView(task);
-
   const replaceCardToForm = () => {
     taskListElement.replaceChild(taskEditComponent.element, taskComponent.element);
   };
@@ -47,6 +45,7 @@ const renderTask = (taskListElement, task) => {
   taskEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
     replaceFormToCard();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   render(taskListElement, taskComponent.element, RenderPosition.BEFOREEND);
@@ -57,34 +56,38 @@ render(siteMainElement, new FilterView(filters).element, RenderPosition.BEFOREEN
 
 const boardComponent = new BoardView();
 render(siteMainElement, boardComponent.element, RenderPosition.BEFOREEND);
-render(boardComponent.element, new SortView().element, RenderPosition.AFTERBEGIN);
 
-const taskListComponent = new TaskListView();
-render(boardComponent.element, taskListComponent.element, RenderPosition.BEFOREEND);
+if (tasks.every((task) => task.isArchive)) {
+  render(boardComponent.element, new NoTaskView().element, RenderPosition.BEFOREEND);
+} else {
+  render(boardComponent.element, new SortView().element, RenderPosition.BEFOREEND);
 
-for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  renderTask(taskListComponent.element, tasks[i]);
-}
+  const taskListComponent = new TaskListView();
+  render(boardComponent.element, taskListComponent.element, RenderPosition.BEFOREEND);
 
-if (tasks.length > TASK_COUNT_PER_STEP) {
-  let renderedTaskCount = TASK_COUNT_PER_STEP;
+  for (let i = 0; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
+    renderTask(taskListComponent.element, tasks[i]);
+  }
 
-  const loadMoreButtonComponent = new LoadMoreButtonView();
+  if (tasks.length > TASK_COUNT_PER_STEP) {
+    let renderedTaskCount = TASK_COUNT_PER_STEP;
 
-  render(boardComponent.element, loadMoreButtonComponent.element, RenderPosition.BEFOREEND);
+    const loadMoreButtonComponent = new LoadMoreButtonView();
 
-  loadMoreButtonComponent.element.addEventListener('click', (evt) => {
-    evt.preventDefault();
+    render(boardComponent.element, loadMoreButtonComponent.element, RenderPosition.BEFOREEND);
 
-    tasks
-      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-      .forEach((task) => renderTask(taskListComponent.element, task));
+    loadMoreButtonComponent.element.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      tasks
+        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+        .forEach((task) => renderTask(taskListComponent.element, task));
 
-    renderedTaskCount += TASK_COUNT_PER_STEP;
+      renderedTaskCount += TASK_COUNT_PER_STEP;
 
-    if (renderedTaskCount >= tasks.length) {
-      loadMoreButtonComponent.element.remove();
-      loadMoreButtonComponent.removeElement();
-    }
-  });
+      if (renderedTaskCount >= tasks.length) {
+        loadMoreButtonComponent.element.remove();
+        loadMoreButtonComponent.removeElement();
+      }
+    });
+  }
 }
