f23edc6cc1a25e6015fc54b690344d1bcb57d794
Alexey <apple230379@gmail.com>
2021-12-26 16:46:37 +0300

    7.1.9 Добавит презентер фильтров - По образу презентера доски создадим презентер фильтров - В презентер фильтров передадим модель фильтров и подпишем презентер на ее изменения - Научим презентер считать количество задач и формировать данные для представления (геттер filters) - Используем презентер в точке входа
    N.B. Благодаря тому, что 'рельсы датабиндинга' в приложении уже настроены, мы с легкостью передаем данные от представления к модели через презентер и обратно

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import SiteMenuView from './view/site-menu-view.js';
-import FilterView from './view/filter-view.js';
 import {render, RenderPosition} from './utils/render.js';
 import {generateTask} from './mock/task.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TasksModel from './model/tasks-model.js';
 import FilterModel from './model/filter-model.js';
@@ -9,13 +9,6 @@ import FilterModel from './model/filter-model.js';
 const TASK_COUNT = 22;
 
 const tasks = Array.from({length: TASK_COUNT}, generateTask);
-const filters = [
-  {
-    type: 'all',
-    name: 'ALL',
-    count: 0,
-  },
-];
 
 const tasksModel = new TasksModel();
 tasksModel.tasks = tasks;
@@ -25,9 +18,11 @@ const filterModel = new FilterModel();
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
-const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
-
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterView(filters, 'all'), RenderPosition.BEFOREEND);
+
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
+const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
+
+filterPresenter.init();
 
 boardPresenter.init();
